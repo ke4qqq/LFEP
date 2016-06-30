5bd1254306da35f47461001f2d61094b9e1d9f21
David Nalley <david@gnsa.us>
2016-06-30 18:33:18 -0400

    first pass

---
+++ Python_the_dog.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import xlrd
+import csv
+
+def csv_from_excel():
+
+    wb = xlrd.open_workbook('Sched.xlsx')
+    sh = wb.sheet_by_index(0)
+    your_csv_file = open('your_csv_file.csv', 'wb')
+    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
+
+    for rownum in xrange(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    your_csv_file.close()
