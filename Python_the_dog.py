cbeacf96404f9069858e22516756340f57bf743d
David Nalley <david@gnsa.us>
2016-06-30 19:02:07 -0400

    some updates

--- Python_the_dog.py
+++ Python_the_dog.py
@@ -7,7 +7,7 @@ def csv_from_excel():
 
     wb = xlrd.open_workbook('Sched.xlsx')
     sh = wb.sheet_by_index(0)
-    your_csv_file = open('your_csv_file.csv', 'wb')
+    your_csv_file = open('1.csv', 'wb')
     wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
 
     for rownum in xrange(sh.nrows):
@@ -15,4 +15,16 @@ def csv_from_excel():
 
     your_csv_file.close()
 
+
+def rm_hdr_lines_from_sched():
+    #the sched spreadsheet has some bogus instruction lines that aren't 
+    # necessary for our purposes - please nuke them for orbit
+    with open('1.csv', 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open('1.csv', 'w') as fout:
+        fout.writelines(data[8:])
+
+
 csv_from_excel()
+rm_hdr_lines_from_sched()
+
